500acab57c3d650e5b2edabe3cd454ff89626d4c
Gandino1984 <andinogerman@gmail.com>
2024-10-29 09:54:05 +0100

    clean up code top bar buttons

--- vite-project/src/landing_page/ContactButton.jsx
+++ vite-project/src/landing_page/ContactButton.jsx
@@ -5,34 +5,17 @@ import './ContactButton.css';
 
 
 const ContactButton = ({ icon, isActive, onClick }) => {
-  const [isHovered, setIsHovered] = useState(false);
 
-  const buttonSpring = useSpring({
-    transform: isActive ? 'scale(1.2)' : 'scale(1)',
-    backgroundColor: isActive ? 'var(--purple)' : (isHovered ? 'black' : 'var(--gray)'),
-    borderColor: isHovered && !isActive ? 'var(--purple)' : 'transparent',
-    borderWidth: '2px',
-    borderStyle: 'solid',
-    config: config.wobbly
-  });
-
-  const iconSpring = useSpring({
-    color: isActive ? 'white' : (isHovered ? 'var(--purple)' : 'inherit'),
-  });
 
   return (
-    <animated.button 
-      className={`contact-button ${isActive ? 'active' : ''}`}
-      style={buttonSpring}
+    <button 
+      className={`contact-button`}
       onClick={onClick}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
       aria-label="contact button"
     >
-      <animated.div style={iconSpring}>
+      
         <ion-icon name={icon}></ion-icon>
-      </animated.div>
-    </animated.button>
+    </button>
   );
 };
 
